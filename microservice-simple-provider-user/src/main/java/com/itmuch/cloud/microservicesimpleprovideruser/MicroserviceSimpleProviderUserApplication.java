6c3b438b0578bde089fb2d30ebfb42471855ee6a
LinJK <linjk121@163.com>
2017-07-18 11:25:34 +0800

    service test OK.

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -3,7 +3,17 @@ package com.itmuch.cloud.microservicesimpleprovideruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 调用方式: http://localhost:8000/{id}
+ * */
+
 @SpringBootApplication
+/**
+ * SoringBootApplication是如下三个注解的组合注解:
+ * 	1. @Configuration
+ * 	2. @EnableAutoConfiguration
+ * 	3. @ComponentScan
+ * */
 public class MicroserviceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {
