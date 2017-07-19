e1643fb07e20bbe38dbe84c3adcf58bafcb08b44
LinJK <linjk@zhenglaikeji.com>
2017-07-19 14:36:53 +0800

    引入Ribbon负载均衡，在starter-eureka已包含ribbon模块，直接使用即可，完成

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
@@ -1,9 +1,12 @@
 package com.itmuch.cloud.microservicesimpleconsumermovie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +19,14 @@ import java.util.List;
  */
 @RestController
 public class MovieController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
     @Autowired
     private RestTemplate restTemplate;
     @Autowired
     private DiscoveryClient discoveryClient;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
     @Value("${user.userServiceUrl}")
     private String userServiceUrl;
@@ -32,5 +39,12 @@ public class MovieController {
     @GetMapping("/user-instance")
     public List<ServiceInstance> showInfo() {
         return this.discoveryClient.getInstances("microservice-discovery-eureka");
+    }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-simple-provider-user");
+        MovieController.LOGGER.info("{}:{}:{}",
+                serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 }
