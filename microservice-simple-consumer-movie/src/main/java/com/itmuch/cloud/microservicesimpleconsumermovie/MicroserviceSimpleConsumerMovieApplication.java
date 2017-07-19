97d673e48fd643910936a1e2172454ef0457c295
LinJK <linjk@zhenglaikeji.com>
2017-07-19 11:29:29 +0800

    电影微服务注册到服务注册中心完成

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -2,13 +2,14 @@ package com.itmuch.cloud.microservicesimpleconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * 调用方式: http://localhost:8010/user/{id}
  * */
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceSimpleConsumerMovieApplication {
 	@Bean
