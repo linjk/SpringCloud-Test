49bacc3252178e03d2329750577cd1173fe20472
LinJK <linjk@zhenglaikeji.com>
2017-07-19 13:48:27 +0800

    Eureka的自定义元数据测试，完成

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
@@ -2,10 +2,14 @@ package com.itmuch.cloud.microservicesimpleconsumermovie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by admin on 2017-07-18.
@@ -14,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
 public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @Value("${user.userServiceUrl}")
     private String userServiceUrl;
@@ -21,5 +27,10 @@ public class MovieController {
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
         return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return this.discoveryClient.getInstances("microservice-discovery-eureka");
     }
 }
