851f73537b49c88b8cf226033cc51a43df67113d
LinJK <linjk@zhenglaikeji.com>
2017-07-19 11:26:28 +0800

    客户微服务注册到服务注册中心完成

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -2,11 +2,12 @@ package com.itmuch.cloud.microservicesimpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 调用方式: http://localhost:8000/{id}
  * */
-
+@EnableDiscoveryClient
 @SpringBootApplication
 /**
  * SoringBootApplication是如下三个注解的组合注解:
