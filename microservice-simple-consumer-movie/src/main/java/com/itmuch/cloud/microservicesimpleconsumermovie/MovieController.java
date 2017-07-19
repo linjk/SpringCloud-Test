b879ae19030a0d59a5e2363a2f3bb4a89536385d
LinJK <linjk@zhenglaikeji.com>
2017-07-19 10:45:57 +0800

    1. 增加auctorar监控模块支持，并在yml增加info节点的输出信息 2. movice模块对user模块的调用地址信息放到yml文件来处理，使用@Value获取值

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
@@ -1,6 +1,7 @@
 package com.itmuch.cloud.microservicesimpleconsumermovie;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,11 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${user.userServiceUrl}")
+    private String userServiceUrl;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+        return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
     }
 }
