02c394f54571a33e7be78c93f6b0f16ac5fae3a3
LinJK <linjk@zhenglaikeji.com>
2017-07-20 08:54:47 +0800

    利用Feign改写电影微服务对用户微服务的电影，使用接口声明url的方式

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,13 +13,8 @@ import org.springframework.web.client.RestTemplate;
  * */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class MicroserviceSimpleConsumerMovieApplication {
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
 	}
--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
@@ -1,16 +1,11 @@
 package com.itmuch.cloud.microservicesimpleconsumermovie;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -19,32 +14,19 @@ import java.util.List;
  */
 @RestController
 public class MovieController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
-
-    @Autowired
-    private RestTemplate restTemplate;
     @Autowired
     private DiscoveryClient discoveryClient;
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
 
-    @Value("${user.userServiceUrl}")
-    private String userServiceUrl;
+    @Autowired
+    private UserFeignClient userFeignClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
+        return this.userFeignClient.findById(id);
     }
 
     @GetMapping("/user-instance")
     public List<ServiceInstance> showInfo() {
         return this.discoveryClient.getInstances("microservice-discovery-eureka");
-    }
-
-    @GetMapping("/log-instance")
-    public void logUserInstance() {
-        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-simple-provider-user");
-        MovieController.LOGGER.info("{}:{}:{}",
-                serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 }
