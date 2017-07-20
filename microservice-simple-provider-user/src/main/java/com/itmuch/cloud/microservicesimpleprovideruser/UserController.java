27df3d25661b0826cec008f80824ae080593ea25
LinJK <linjk@zhenglaikeji.com>
2017-07-20 14:41:16 +0800

    用户微服务根据不同用户和角色配置不同业务逻辑，后面根据电影微服务调用来提供，完成

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/UserController.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/UserController.java
@@ -1,10 +1,17 @@
 package com.itmuch.cloud.microservicesimpleprovideruser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 
 /**
  * Created by admin on 2017-07-18.
@@ -12,10 +19,23 @@ import java.math.BigDecimal;
 @RestController
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
-//        User user = this.userRepository.findOne(id);
-//        return user;
+        Object pricipal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (pricipal instanceof UserDetails) {
+            UserDetails user = (UserDetails)pricipal;
+            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
+
+            for (GrantedAuthority c : collection) {
+                UserController.LOGGER.info("当前用户{}，角色{}", user.getUsername(), c.getAuthority());
+            }
+        }
+        else {
+            // do other thing.
+        }
 
         // 这里没有采用数据库，直接新建一个对象赋值处理了
         User user = new User();
---
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/WebSecurityConfig.java
@@ -0,0 +1,151 @@
+package com.itmuch.cloud.microservicesimpleprovideruser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by admin on 2017-07-20.
+ */
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 所有的请求，都需要经过HTTP basic认证
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 明文编码器。这是一个不做任何操作的密码编码器，事Spring提供做明文测试的
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder());
+    }
+
+    @Component
+    class CustomUserDetailsService implements UserDetailsService {
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            if ("linjk".equals(username)) {
+                System.out.println("input linjk...");
+                return new SecurityUser("linjk", "ljk121", "user-role");
+            }
+            else if ("admin".equals(username)) {
+                System.out.println("input admin...");
+                return new SecurityUser("admin", "ljk121121", "admin-role");
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    class SecurityUser implements UserDetails {
+        private static final long serialVersionUID = 1L;
+
+        public SecurityUser(String username, String password, String role) {
+            super();
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+        public SecurityUser() {
+            //
+        }
+
+        private Long id;
+        private String username;
+        private String password;
+        private String role;
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.role);
+            authorities.add(authority);
+
+            return authorities;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+    }
+}
