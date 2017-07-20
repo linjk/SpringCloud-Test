338e1b56b3838e01be24aacdfd0b3bc9a44dce99
LinJK <linjk@zhenglaikeji.com>
2017-07-20 17:41:43 +0800

    增加hystrix dashboard页面url说明

--- microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/microservicehystrixdashboard/MicroserviceHystrixDashboardApplication.java
+++ microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/microservicehystrixdashboard/MicroserviceHystrixDashboardApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+/**
+ * 监控页面: http://localhost:8030/hystrix
+ * */
+
 @SpringBootApplication
 @EnableHystrixDashboard
 public class MicroserviceHystrixDashboardApplication {
