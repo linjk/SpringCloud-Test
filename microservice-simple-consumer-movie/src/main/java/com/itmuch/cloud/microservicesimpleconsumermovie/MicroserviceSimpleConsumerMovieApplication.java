ae7b6334d7dfe0914399486320df25172bb4a674
LinJK <linjk@zhenglaikeji.com>
2017-07-20 17:35:41 +0800

    增加hystrix dashboard监控页面

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -3,11 +3,7 @@ package com.itmuch.cloud.microservicesimpleconsumermovie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 调用方式: http://localhost:8010/user/{id}
