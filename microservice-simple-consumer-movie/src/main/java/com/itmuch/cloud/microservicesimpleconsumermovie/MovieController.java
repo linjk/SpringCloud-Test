a81bd45f2cf835c3c89d62da540c403afdba6b1a
LinJK <linjk@zhenglaikeji.com>
2017-07-20 16:45:25 +0800

    电影微服务加入断路器，当用户微服务不可用时，调用回退方法。

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MovieController.java
@@ -1,5 +1,7 @@
 package com.itmuch.cloud.microservicesimpleconsumermovie;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import feign.Client;
 import feign.Contract;
 import feign.Feign;
@@ -12,6 +14,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 /**
  * Created by admin on 2017-07-18.
@@ -42,13 +46,46 @@ public class MovieController {
                 .target(UserFeignClient.class, "http://microservice-simple-provider-user/");
     }
 
+    @HystrixCommand(fallbackMethod = "findByIdUserFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000")
+    }, threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "1"),
+            @HystrixProperty(name = "maxQueueSize", value = "10"),
+    })
     @GetMapping("/user-user/{id}")
     public User findByIdUser(@PathVariable Long id) {
         return this.userFeignClient.findById(id);
     }
 
+    public User findByIdUserFallback(Long id) {
+        return getErrorUser();
+    }
+
+    @HystrixCommand(fallbackMethod = "findByIdAdminFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000")
+    }, threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "1"),
+            @HystrixProperty(name = "maxQueueSize", value = "10"),
+    })
     @GetMapping("/user-admin/{id}")
     public User findByIdAdmin(@PathVariable Long id) {
         return this.adminFeignClient.findById(id);
+    }
+
+    public User findByIdAdminFallback(Long id) {
+        return getErrorUser();
+    }
+
+    private User getErrorUser() {
+        User user = new User();
+        user.setId(-1L);
+        user.setUsername("Error");
+        user.setName("Error");
+        user.setAge(-1);
+        user.setBalance(BigDecimal.valueOf(-1L));
+
+        return user;
     }
 }
